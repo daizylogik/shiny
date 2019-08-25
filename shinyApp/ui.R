d223ee380f29ed166f23d08ad9863a5c19c8c005
daizylogik <vladimir@daizylogik.com>
2019-08-25 07:28:41 -0700

    updates

--- shinyApp/ui.R
+++ shinyApp/ui.R
@@ -11,20 +11,20 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
+    
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
+    titlePanel("Cars Data"),
+    
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
                         "Number of bins:",
                         min = 1,
-                        max = 50,
-                        value = 30)
+                        max = 10,
+                        value = 5)
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot")
